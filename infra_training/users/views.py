cb3b36d22ca8955d3af2efd6679dbd0f4dfdfa65
Ivaylo Bachvarof <bachvarof@gmail.com>
2018-10-13 10:21:55 +0300

    Add cpu view

--- infra_training/users/views.py
+++ infra_training/users/views.py
@@ -1,52 +1,10 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
-from django.views.generic import DetailView, ListView, RedirectView, UpdateView
+import random
+import itertools
 
-User = get_user_model()
+from django.http import HttpResponse
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
-
-    model = User
-    slug_field = "username"
-    slug_url_kwarg = "username"
-
-
-user_detail_view = UserDetailView.as_view()
-
-
-class UserListView(LoginRequiredMixin, ListView):
-
-    model = User
-    slug_field = "username"
-    slug_url_kwarg = "username"
-
-
-user_list_view = UserListView.as_view()
-
-
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-
-    model = User
-    fields = ["name"]
-
-    def get_success_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
-
-    def get_object(self):
-        return User.objects.get(username=self.request.user.username)
-
-
-user_update_view = UserUpdateView.as_view()
-
-
-class UserRedirectView(LoginRequiredMixin, RedirectView):
-
-    permanent = False
-
-    def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
-
-
-user_redirect_view = UserRedirectView.as_view()
+def cpu_view(request):
+    random_numbers = [random.randint(1, 100) * random.randint(300, 6000) for x in range(600000)]
+    itertools.permutations(random_numbers)
+    return HttpResponse('<h1>Page was found</h1>')
